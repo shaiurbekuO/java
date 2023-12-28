86a0f746f3a3f60c296c38c37e39f8bffb08e367
Omurbek <114275896+shaiurbekuO@users.noreply.github.com>
2023-12-28 14:12:29 +0600

    Add files via upload

---
+++ Stepik/3-4-Оператор-Switch.java
@@ -0,0 +1,14 @@
+class MyNumber {
+    public static void main(String[] args) {
+        int x = 10; 
+        switch (x){    
+
+            case 10:
+                System.out.print("We're the champions!");
+                break;
+
+            default:
+                System.out.print("Were lost ");
+        }           
+    }
+}
