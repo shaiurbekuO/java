6c754ff2a739ae5b8cf7f37b0197628e868dd31f
Omurbek <114275896+shaiurbekuO@users.noreply.github.com>
2024-01-04 17:07:20 +0600

    1.8 Структуры данных. Массивы 1

---
+++ Stepik/1-8-Структуры-данных-Массивы-1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[sc.nextInt()];
+		for(int i=0; i<arr.length; i++){
+		    arr[i] = sc.nextInt();
+		}
+		int n1 = sc.nextInt();
+		for(int i=0; i<arr.length; i++){
+		    System.out.println(arr[i]*n1);
+		}
+		
+	}
+}
