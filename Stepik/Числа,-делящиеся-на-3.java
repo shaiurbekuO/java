68b055fa83037bd3f949d5da82705442f0a067d9
Omurbek <114275896+shaiurbekuO@users.noreply.github.com>
2024-05-17 22:01:33 +0600

    Числа, делящиеся на 3

---
+++ Stepik/Числа,-делящиеся-на-3.java
@@ -0,0 +1,6 @@
+class Main {
+    public static void main(String[] args) {
+        int k = new java.util.Scanner(System.in).nextInt();
+        System.out.print(k == k / 3 * 3 ? "YES" : "NO");
+    }
+}
